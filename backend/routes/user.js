03d369cef24b5cfdbdcdd8a6b4c61f04c8ce9f8d
Dev Mer <138443145+devmer2311@users.noreply.github.com>
2024-11-17 00:04:08 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,11 @@ const express = require('express');
 const { signup, login } = require('../controllers/authController');
 const router = express.Router();
 
+router.post('/signup', signup);
+router.post('/login', login);
+
+
+
 /**
  * @swagger
  * /users/signup:
